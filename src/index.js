96b507e82239be3498ebccc65d7f3b12b8cd3a2a
AlexAirCG <alexairggg@yandex.ru>
2026-04-01 21:28:04 +0300

    added index.js refactoring even-game.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import readlineSync from "readline-sync";
+
+const roundCount = 3;
+
+const runGame = (discription, getRoundData) => {
+  console.log("Welcome to the Brain Games!");
+  const userName = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${userName}!`);
+
+  console.log(discription);
+
+  for (let i = 0; i < roundCount; i += 1) {
+    const [question, correctAnswer] = getRoundData();
+
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question("Your answer: ");
+
+    if (userAnswer !== correctAnswer) {
+      console.log(
+        `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}`,
+      );
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log("Correct!");
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default runGame;
